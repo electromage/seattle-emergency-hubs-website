747a9a3ef6e83ec6bbe8b2d72f990175e6e9e7ec
electromage <electromage@gmail.com>
2026-05-28 19:33:02 -0700

    add youtube link to footer

--- js/components.js
+++ js/components.js
@@ -105,6 +105,7 @@
         <a href="${prefix}map.html">Neighborlink Map</a>
         <a href="${prefix}hubs/index.html">Hubs</a>
         <a href="${prefix}resources.html">Resources</a>
+        <a href="https://www.youtube.com/c/SeattleemergencyhubsOrg2015" target="_blank" rel="noopener noreferrer">YouTube</a>
       </nav>
     </div>
 
